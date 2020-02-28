53b810ab8a3c83df5584cc1af0bc1076eb4e3c35
mariazhy <44722353+mariazhy@users.noreply.github.com>
2020-02-28 14:39:48 +0200

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorProj
+{
+    public class Calculator
+    {
+        public int SumNumbers(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int MultiplyNumbers(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int SubstractNumbers(int a, int b)
+        {
+            return a - b;
+        }
+
+        public double DivideNumbers(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ CalculatorTests.cs
@@ -0,0 +1,64 @@
+﻿using CalculatorProj;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorTests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        Calculator calculator = new Calculator();
+        
+        [Test, Description("Sum of two numbers")]
+        public void CheckSumOfNumbers_01()
+        {
+            Assert.AreEqual(20, calculator.SumNumbers(10, 10));
+        }
+
+        [Test, Description("Sum of two numbers")]
+        public void CheckSumOfNumbers_02()
+        {
+            Assert.AreEqual(15, calculator.SumNumbers(10, 10));
+        }
+
+        [Test, Description("Multiply numbers")]
+        public void CheckMultiplyOfNumbers_01()
+        {
+            Assert.AreEqual(20, calculator.MultiplyNumbers(4, 5));
+        }
+
+        [Test, Description("Multiply numbers")]
+        public void CheckMultiplyOfNumbers_02()
+        {
+            Assert.AreEqual(15, calculator.MultiplyNumbers(4, 5));
+        }
+
+        [Test, Description("Multiply numbers")]
+        public void ChekSubstractOfNumbers_01()
+        {
+            Assert.AreEqual(20, calculator.SubstractNumbers(50, 30));
+        }
+
+        [Test, Description("Multiply numbers")]
+        public void ChekSubstractOfNumbers_02()
+        {
+            Assert.AreEqual(15, calculator.SubstractNumbers(50, 30));
+        }
+
+        [Test, Description("Multiply numbers")]
+        public void CheckDivideOfNumbers_01()
+        {
+            Assert.AreEqual(20, calculator.DivideNumbers(40, 2));
+        }
+
+        [Test, Description("Multiply numbers")]
+        public void CheckDivideOfNumbers_02()
+        {
+            Assert.AreEqual(15, calculator.DivideNumbers(40, 2));
+        }
+    }
+}
